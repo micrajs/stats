3b93291cc6b449c6b6645635db8b08921678ee3f
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-15 17:24:39 -0500

    chore: started consuming @micra/vite-config

--- .viterc.ts
+++ .viterc.ts
@@ -1,52 +1,5 @@
-/// <reference types="vitest" />
-import { join } from 'path';
-import { defineConfig } from 'vite';
-
-const cwd = (...path: string[]) => join(process.cwd(), ...path);
+import { defineConfig } from '@micra/vite-config/library';
 
 export default defineConfig({
-  build: {
-    outDir: cwd('.'),
-    lib: {
-      entry: cwd('index.ts'),
-      formats: ['cjs', 'es'],
-      fileName: (format) => {
-        if (format === 'cjs') {
-          return `[name].js`;
-        }
-        if (format === 'es') {
-          return `[name].mjs`;
-        }
-        return `[name].${format}.js`;
-      },
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        assetFileNames: '[name].[ext]',
-        preserveModules: true,
-        globals: {},
-      },
-    },
-  },
-
-  resolve: {
-    alias: {
-      '@': cwd(),
-    },
-  },
-
-  plugins: [
-    //
-  ],
-
-  test: {
-    global: true,
-    coverage: {
-      excludeNodeModules: true,
-      reporter: ['json-summary'],
-      reportsDirectory: 'coverage',
-      exclude: ['.*.*'],
-    },
-  },
+  plugins: [],
 });
