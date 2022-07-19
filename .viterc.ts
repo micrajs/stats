4dd76d465905752c39f7ed28b14fe7004e33e9d7
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-07-18 21:15:10 -0500

    chore: updated dependencies and bundler config

--- .viterc.ts
+++ .viterc.ts
@@ -1,5 +1,15 @@
-import { defineConfig } from '@micra/vite-config/library';
+import {defineConfig} from '@micra/vite-config/library';
+import {cwd} from '@micra/vite-config/utilities/cwd';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [],
+      input: {
+        index: cwd('index.ts'),
+      },
+    },
+  },
+
   plugins: [],
 });
